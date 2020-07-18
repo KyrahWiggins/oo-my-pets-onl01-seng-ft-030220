a989d0a4f3ab70bc9ceb057dadc108d06690612d
Kyrah Wiggins <kyrah.wiggins@gmail.com>
2020-07-18 07:43:51 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,13 +1,8 @@
 class Dog
-
   attr_accessor :mood
+  attr_reader :name
 
   def initialize(name)
     @name = name
     @mood = "nervous"
   end
-
-  def change_mood(mood)
-    @mood = mood
-  end
-end
