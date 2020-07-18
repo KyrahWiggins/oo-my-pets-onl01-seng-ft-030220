7ba00e115426d34a9e87d9d9fd055d2607b02bd0
Kyrah Wiggins <kyrah.wiggins@gmail.com>
2020-07-18 07:46:31 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,64 +1,70 @@
 class Owner
- attr_reader :species
-  attr_accessor :name, :pets
-  @@all = []
+OWNERS = [] 
+attr_accessor :name, :pets 
+attr_reader :species 
 
-  def initialize(species, name = nil)
-    @species = species
-    @name = name
-    @pets = {:dogs=> [], :cats=> []}
-    @@all << self
-  end
+def initialize(species)
+  @species = species 
+  OWNERS << self 
+  @pets = {:dogs => [], :cats => []}
+end 
 
-  def self.all
-    @@all
-  end
+def self.all ##Do we have to use this if we use a class variable? @@all 
+OWNERS 
+end 
 
-  def self.count
-    @@all.count
-  end
+def self.reset_all 
+OWNERS.clear 
+end 
 
-  def self.reset_all
-    @@all.clear
-  end
+def self.count
+OWNERS.size 
+end
 
-  def say_species
-    "I am a #{@species}."
-  end
+def species 
+  @species 
+end 
 
-  def buy_cat(name)
-    new_cat = Cat.new(name)
-    @pets[:cats] << new_cat
-  end
+def say_species 
+  return "I am a #{species}"
+end 
+  
+def name 
+  @name #where did we set @name = name? 
+end 
 
-  def buy_dog(name)
-    new_dog = Dog.new(name)
-    @pets[:dogs] << new_dog
-  end
+def buy_cat(name)
+  pets[:cats] << Cat.new(name)
+end 
 
-  def walk_dogs
-    @pets[:dogs].collect do |dog|
-      dog.change_mood("happy")
-    end
-  end
+def buy_dog(name)
+  pets[:dogs] << Dog.new(name)
+end 
 
-  def play_with_cats
-    @pets[:cats].collect do |cat|
-      cat.change_mood("happy")
-    end
-  end
+def walk_dogs
+  pets[:dog].each do |dog|
+    dog.mood = "happy"
+  end 
+end 
 
-
-  def sell_pets
-    @pets[:dogs].collect do |dog|
-      dog.change_mood("nervous")
-    end
-    @pets[:cats].collect do |cat|
-      cat.change_mood("nervous")
-    end
+def play_with_cats
+  pets[:cats].each do |cat| 
+  cat.mood = "Happy"
+  end 
+end 
 
 
-  def list_pets
-    return "I have #{@pets[:dogs].size} dog(s), and #{@pets[:cats].size} cat(s)."
-  end
+def list_pets
+  "I have #{I have #{pets[:cats].count} cats. I have #{pets[:dogs].count} dogs."
+end 
+
+end #finish Owner 
+
+
+
+def initialize 
+  @name = name 
+  @mood = "nervous"
+end 
+
 end 
